0e9333e2eaf357606b446daef06dbdf5cb9c80e0
TimOsahenru <timosahenru@gmail.com>
2025-03-05 22:46:46 +0000

    event app created

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 @pytest.fixture(scope="module")
 def browser():
     # Initialize the Chrome browser
