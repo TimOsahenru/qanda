f17dc9fad834a34f0caf48bb8a63737a40f45a6b
TimOsahenru <timosahenru@gmail.com>
2025-03-06 20:29:08 +0000

    home page created

--- events/tests/test_selenium.py
+++ events/tests/test_selenium.py
@@ -7,10 +7,17 @@ from selenium.webdriver.common.by import By
 def test_home_page(browser):
     browser.get('http://localhost:8000/')
 
-    assert 'install' in browser.title
-# 
-    heading = browser.find_element('tag name', 'h1')
-    assert heading.text == 'The install worked successfully! Congratulations!'
+    title_section = browser.find_element(By.TAG_NAME, 'title')
+    title_name = title_section.get_attribute('textContent')
+    assert title_name == 'qanda | home', f"Expected title to be 'qanda | home' but {title_name}"
 
+
+    # heading = browser.find_element('tag name', 'h1')
+    # assert heading.text == 'The install worked successfully! Congratulations!'
+
+    # form_section = browser.find_element(By.TAG_NAME, 'qanda | home') 
     # test event form
     # test for redirect
+    # test title
+    # test button name
+    # test taglines in hero section
