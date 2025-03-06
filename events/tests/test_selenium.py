97ff1a3e9d13610e672246e75a45af17035db21a
TimOsahenru <timosahenru@gmail.com>
2025-03-06 21:50:01 +0000

    selenium test passing

--- events/tests/test_selenium.py
+++ events/tests/test_selenium.py
@@ -11,10 +11,25 @@ def test_home_page(browser):
     title_name = title_section.get_attribute('textContent')
     assert title_name == 'qanda | home', f"Expected title to be 'qanda | home' but {title_name}"
 
+    hero_section = browser.find_element(By.CSS_SELECTOR, 'div.detail-box h1')
+    hero_title = hero_section.get_attribute('textContent').strip()
+    assert hero_title == 'Smartest way to collect questions from your audience.'
+
+    hero_section = browser.find_element(By.CSS_SELECTOR, 'div.detail-box p')
+    hero_title = hero_section.get_attribute('textContent').strip()
+    assert hero_title == 'Streamline your Q&A in advance. Just share the link with your audience'
+
     form_section = browser.find_element(By.TAG_NAME, 'form') 
 
-    # test event form
-    # test for redirect
+    event_name_input = browser.find_element(By.CSS_SELECTOR, '.input-field')
+    event_name_input.send_keys('PyConGh 2025')
+    submit_button = browser.find_element(By.CSS_SELECTOR, '.btn1')
+    submit_button.click()
+    redirect_url = 'http://localhost:8000/question'
+    assert browser.current_url == redirect_url
+
+    # test event form [x]
+    # test for redirect [x]
     # test title [x]
-    # test button name
+    # test button name [x]
     # test taglines in hero section
