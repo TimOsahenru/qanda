17117ba03b75d7ecc49dbb3b1b6fe2256a31b65f
TimOsahenru <timosahenru@gmail.com>
2025-03-11 18:15:00 +0000

    created and update fields rectified with appropriate fields

--- events/models.py
+++ events/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Event(models.Model):
     # organizer
     name = models.CharField(max_length=250)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     url = models.URLField()
     # questions
 
